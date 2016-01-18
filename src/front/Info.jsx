a447c885d7bc2b0354d751ee0854a38108156220
Adam Sokolnicki <adam.sokolnicki@gmail.com>
2016-01-18 16:35:02 +0100

    Add onRequestClose prop to the Info component

--- src/front/Info.jsx
+++ src/front/Info.jsx
@@ -30,6 +30,7 @@ var Component = React.createClass({
       <Snackbar message={this.state.message}
                 action={this.state.action}
                 onActionTouchTap={this.onActionTouchTap}
+                onRequestClose={this.ok}
                 style={{zIndex: '100'}}
                 ref="snack"
                 open={this.state.open} />
